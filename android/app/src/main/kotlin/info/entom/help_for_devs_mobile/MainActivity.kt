9b2cb07916ea9e6d2d921a6994f1be1059e50ddc
entom <tomasz@entom.info>
2019-03-15 23:01:24 +0100

    Initial project structure

---
+++ android/app/src/main/kotlin/info/entom/help_for_devs_mobile/MainActivity.kt
@@ -0,0 +1,13 @@
+package info.entom.help_for_devs_mobile
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
